a6217aad1a25a8d690196f23346ef68ee126a18f
EdgarVelasco1 <edgar.velasco@epn.edu.ec>
2024-07-17 12:40:43 -0500

    fat arrow function render y remover childs desde parent

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 // console.log('estoy aqui escuchando')
 const todos = []
 
+const render = () => {
+    const todoList = document.getElementById('todo-list')
+    const todosPlantilla = todos.map(t => '<li>'+t+'</li>')
+    todoList.innerHTML = todosPlantilla.join('')
+
+    /*
+    ahora vamos a almacenar la lista, para ello leemos desde el html
+    */
+
+    const elementosParaGuardar = document.querySelectorAll('#todo-list li')
+    elementosParaGuardar.forEach((elemento,i) => {
+        elemento.addEventListener('click', () => {
+            // console.log(elemento)
+            elemento.parentNode.removeChild(elemento) // pero tambien necesitamos sacar el index del arreglo
+            todos.splice(i,1)//para solucionar que los indices no se actualizan, hay que llamar nuevamente a render
+            render()
+        })
+    })
+    const totalTodos = document.getElementById('total-todos')
+    const totalTodosPlantilla = '<label>'+ 'N° Elementos:' + todos.length +'</label>'
+    totalTodos.innerHTML = totalTodosPlantilla
+
+}
+
 window.onload = () => {
+    render()
     const form = document.getElementById('todo-form')
 
     form.onsubmit = (e) => {
@@ -11,10 +36,6 @@ window.onload = () => {
         todo.value = ''
 
         todos.push(todoText)
-
-        const todoList = document.getElementById('todo-list')
-        const todosPlantilla = todos.map(t => '<li>'+t+'</li>')
-        todoList.innerHTML = todosPlantilla.join('')
-
+        render()
     }
 }
