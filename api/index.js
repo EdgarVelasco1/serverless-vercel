788e095e89904773d35f8f7e5162f0c76878d493
EdgarVelasco1 <edgar.velasco@epn.edu.ec>
2024-07-19 12:35:59 -0500

    modificar index.js en api

--- api/index.js
+++ api/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get(('*'),(request, response) => {
-    console.log('mi primera aplicación serverless')
     response.send({message:'hola mundo'})
+    console.log('mi primera aplicación serverless')
 })
+
+// module.exports = (req, res) => {
+//     res.status(200).json({ message: "Hello, world!" });
+//   };
 
 module.exports = app
